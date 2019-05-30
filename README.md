537d4c237f736821eecba7338bc05f558cf2ac0e
dwilliams2 <dwilliams1@westinghousehs.org>
2019-05-30 19:53:00 +0000

    updates

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Project Name
-
+Movies and Fun
 ## Team Members
-
+Me
 ## Problem
-
-## Opportunity
-
+You love movies.  Can’t get enough of them.  Unfortunately you have a very poor memory.  You need an app that helps you find movies by their title.
 ## Technical Magic
-
+Gitpod,Html,Css,JavaScript
 ## Future Vision
+To help the use to find the movies, that they was look for.
--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+
+$('#search').click(function(){
+    $.ajax({
+        url: 'https://www.omdbapi.com/?apikey=8e9ded79&y=2016&s=dory',
+        method: 'GET',
+        success: function(response){
+            console.log(response);
+            response.Search.forEach(function(movie){
+                console.log(movie);
+                var title = movie.Title
+                html = `
+                <p>${title}</p>
+                `
+                $('body').append(html)
+            })
+        }
+    })
+})
