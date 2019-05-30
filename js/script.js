42a17087b5fe7ec8da5e65b00fb8d66685d8a60e
dwilliams2 <dwilliams1@westinghousehs.org>
2019-05-30 21:02:10 +0000

    updates movies

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 /* global $ */
 
-console.log('hi');
+
 
 $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
-
+var movies = $("#movies").val();
+var years = $("#yearSeach").val();
+var url=`https://www.omdbapi.com/?apikey=8e9ded79&y=2016&s=${movies}`;
 $('#search').click(function(){
+    var movies = $("#movies").val();
+    console.log(movies);
     $.ajax({
-        url: 'https://www.omdbapi.com/?apikey=8e9ded79&y=2016&s=dory',
+        url: `https://www.omdbapi.com/?apikey=8e9ded79&y=2016&s=${movies}`,
         method: 'GET',
         success: function(response){
             console.log(response);
